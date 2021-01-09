rust-mobile/cargo-subcommand

commit bec3883b4b6a9633a39905ee312a273152baff7e

subcommand: Listen to CARGO_(BUILD_)TARGET_DIR over cwd (#4)
If this environment variable is set it should be used over the current
or workspace directory.

Fixes: https://github.com/rust-windowing/android-ndk-rs/issues/102

src/subcommand.rs
@@ -84,6 +84,13 @@ impl Subcommand {
             package.as_ref().map(|s| &**s),
         )?;
         let root_dir = manifest.parent().unwrap();
+
+        let target_dir = target_dir.or_else(|| {
+            std::env::var_os("CARGO_BUILD_TARGET_DIR")
+                .or_else(|| std::env::var_os("CARGO_TARGET_DIR"))
+                .map(|os_str| os_str.into())
+        });
+
         let target_dir = target_dir.unwrap_or_else(|| {
             utils::find_workspace(&manifest, &package)
                 .unwrap()
@@ -92,6 +99,7 @@ impl Subcommand {
                 .unwrap()
                 .join("target")
         });
+        let target_dir = std::fs::canonicalize(target_dir).unwrap();
         if examples {
             for file in utils::list_rust_files(&root_dir.join("examples"))? {
                 artifacts.push(Artifact::Example(file));

commit 82a9b63408bb950e0c8c680ef7e635a1391b2cd4

subcommand: Do not canonicalize `target/` path that may not exist
Canonicalization requires the path to exist such that the OS can resolve
symlinks. The `target/` folder doesn't exist in a clean build
environment (ie. after cloning or running `cargo clean`), causing the
unwrap to panic.

The same logic is applied to paths coming from `--target-dir` which may
or may not be relative. Paths from `find_workspace` (based on those from
`find_package`) are already canonicalized, however.

Tested normal `cargo apk` invocation with and without `target/` dir, and
`CARGO_TARGET_DIR` with relative and absolute path.

Fixes: bec3883 ("subcommand: Listen to CARGO_(BUILD_)TARGET_DIR over cwd (#4)")

src/subcommand.rs
@@ -85,11 +85,19 @@ impl Subcommand {
         )?;
         let root_dir = manifest.parent().unwrap();
 
-        let target_dir = target_dir.or_else(|| {
-            std::env::var_os("CARGO_BUILD_TARGET_DIR")
-                .or_else(|| std::env::var_os("CARGO_TARGET_DIR"))
-                .map(|os_str| os_str.into())
-        });
+        let target_dir = target_dir
+            .or_else(|| {
+                std::env::var_os("CARGO_BUILD_TARGET_DIR")
+                    .or_else(|| std::env::var_os("CARGO_TARGET_DIR"))
+                    .map(|os_str| os_str.into())
+            })
+            .map(|target_dir| {
+                if target_dir.is_relative() {
+                    std::env::current_dir().unwrap().join(target_dir)
+                } else {
+                    target_dir
+                }
+            });
 
         let target_dir = target_dir.unwrap_or_else(|| {
             utils::find_workspace(&manifest, &package)
@@ -99,7 +107,6 @@ impl Subcommand {
                 .unwrap()
                 .join("target")
         });
-        let target_dir = std::fs::canonicalize(target_dir).unwrap();
         if examples {
             for file in utils::list_rust_files(&root_dir.join("examples"))? {
                 artifacts.push(Artifact::Example(file));

src/utils.rs
@@ -35,7 +35,7 @@ fn member(manifest: &Path, members: &[String], package: &str) -> Result<Option<P
 }
 
 pub fn find_package(path: &Path, name: Option<&str>) -> Result<(PathBuf, String), Error> {
-    let path = std::fs::canonicalize(path)?;
+    let path = path.canonicalize()?;
     for manifest_path in path
         .ancestors()
         .map(|dir| dir.join("Cargo.toml"))