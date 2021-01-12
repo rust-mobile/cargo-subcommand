rust-mobile/cargo-subcommand

commit 60fdf8be5f5b7ed75c27ed05b10062c614fc046b

utils: Search for workspace members relative to root manifest (#3)
Workspace members (globs) should be discovered from the directory
containing `Cargo.toml` with these workspaces, not from the directory
the user happens to in.

Take for example android-ndk-rs. When cd'd into `ndk-examples` this
would find the root manifest file in `../Cargo.toml`, but try to glob in
the current directory - `ndk-examples` - where it's obviously not going
to find another `ndk-examples` containing `Cargo.toml`. The `Subcommand`
constructor hence considers this package as not being in a workspace and
emits `target_dir` at `ndk-examples/target`. This fails later in the
build when files cannot be found in the toplevel `target/` dir.

Fixes https://github.com/rust-windowing/android-ndk-rs/issues/105

src/utils.rs
@@ -19,9 +19,10 @@ pub fn list_rust_files(dir: &Path) -> Result<Vec<String>, Error> {
 }
 
 fn member(manifest: &Path, members: &[String], package: &str) -> Result<Option<PathBuf>, Error> {
+    let workspace_dir = manifest.parent().unwrap();
     for member in members {
-        for member in glob::glob(member)? {
-            let manifest_path = manifest.parent().unwrap().join(member?).join("Cargo.toml");
+        for manifest_dir in glob::glob(workspace_dir.join(member).to_str().unwrap())? {
+            let manifest_path = manifest_dir?.join("Cargo.toml");
             let manifest = Manifest::parse_from_toml(&manifest_path)?;
             if let Some(p) = manifest.package.as_ref() {
                 if p.name == package {

commit 8bc5fca731c132f35978fcca0818fbf7773005e2

utils: Strip UNC from canonicalized path which is not supported by glob
We recently taught `fn member()` to look for Cargo.toml files in
workspace members (globs) relative to the workspace root (the directory
containing Cargo.toml with the `[workspace]` listing of these members).
This means the path to the workspace root is now concatenated to the
glob path before searching, which didn't happen before.  That path
happens to be canonicalized just at the top of `fn find_package()`
consequently the whole thing falls apart when Windows UNC paths are used
in places they aren't supported, like `glob`.  No paths are returned,
the package isn't found (`Error::ManifestNotFound` below) and
`cargo-apk` is once again broken on Windows.

We have already been discussing to add `dunce`, now is the right time to
get rid of UNC paths in `cargo-submodule` once and for all.

Fixes: 60fdf8b ("utils: Search for workspace members relative to root manifest (#3)")

Cargo.toml
@@ -8,6 +8,7 @@ repository = "https://github.com/dvc94ch/cargo-subcommand"
 license = "ISC"
 
 [dependencies]
+dunce = "1"
 glob = "0.3.0"
 serde = { version = "1.0.118", features = ["derive"] }
 toml = "0.5.8"

src/utils.rs
@@ -35,7 +35,7 @@ fn member(manifest: &Path, members: &[String], package: &str) -> Result<Option<P
 }
 
 pub fn find_package(path: &Path, name: Option<&str>) -> Result<(PathBuf, String), Error> {
-    let path = path.canonicalize()?;
+    let path = dunce::canonicalize(path)?;
     for manifest_path in path
         .ancestors()
         .map(|dir| dir.join("Cargo.toml"))